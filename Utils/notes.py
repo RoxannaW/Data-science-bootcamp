e8906b688b02e330910f1cfc0c491122f1b0ac11
RoxannaW <roxanna_wijtsma@outlook.com>
2020-07-06 19:07:52 +0200

    New files curso week 1

---
+++ Utils/notes.py
@@ -0,0 +1,8 @@
+def clean_strings(strings):
+    result = []
+    for value in strings:
+        value = value.strip()
+        value = re.sub('[!#?]', '', value)
+        value = value.title()
+        result.append(value)
+    return result
